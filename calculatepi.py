7e31b709c75341389a3202cf5cf1788fef630bd2
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-09-30 13:57:36 -0400

    Updated from Brython Server: 9/30/2015 1:57:36 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Dimitri
+Credit: Mr. Dennison
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
